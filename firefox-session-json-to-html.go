6d77f5678810a5a5896281fe58f3af898d980c21
Chris Barts <chbarts@gmail.com>
2021-11-30 13:06:30 -0700

    Implement HTML conversion

--- firefox-session-json-to-html.go
+++ firefox-session-json-to-html.go
@@ -3,15 +3,82 @@ package main
 import (
 	"bufio"
 	"encoding/json"
-//	"flag"
+	"flag"
 	"fmt"
-//	"html"
+	"html"
 	"io/ioutil"
 	"os"
-//	"regexp"
-//	"sort"
-//	"strconv"
-//	"time"
+	"regexp"
+	"sort"
+	"time"
+)
+
+type TimeValue struct {
+	Time *time.Time
+}
+
+func (t TimeValue) String() string {
+	if t.Time != nil {
+		return t.Time.String()
+	}
+
+	return ""
+}
+
+func MakeTime(str string) (time.Time, error) {
+	const fmt = "2006-01-02T15:04:05"
+	reh := regexp.MustCompile(`.+[tT](\d\d)`)
+	rem := regexp.MustCompile(`.+[tT](\d\d):(\d\d)`)
+	ret := regexp.MustCompile(`.+[tT](\d\d):(\d\d):(\d\d)`)
+	rez := regexp.MustCompile(`.+([zZ]|([+\-](\d\d):(\d\d)))`)
+	tnow := time.Now()
+	location := tnow.Location()
+	strs := ""
+	if rez.MatchString(str) {
+		if tm, err := time.Parse(time.RFC3339, str); err != nil {
+			return tnow, err
+		} else {
+			return tm, nil
+		}
+
+	} else if ret.MatchString(str) {
+		strs = str
+	} else if rem.MatchString(str) {
+		strs = str + ":00"
+	} else if reh.MatchString(str) {
+		strs = str + ":00:00"
+	} else {
+		strs = str + "T00:00:00"
+	}
+
+	if tm, err := time.ParseInLocation(fmt, strs, location); err != nil {
+		return tnow, err
+	} else {
+		return tm, nil
+	}
+}
+
+func (t TimeValue) Set(str string) error {
+	if tm, err := MakeTime(str); err != nil {
+		return err
+	} else {
+		*t.Time = tm
+	}
+
+	return nil
+}
+
+var tstart = &time.Time{}
+var tend = &time.Time{}
+
+var (
+	inf    = flag.String("in", "/dev/stdin", "input file in JSON")
+	outf   = flag.String("out", "/dev/stdout", "output file in HTML")
+	drange = flag.Bool("range", false, "print range of dates represented in the dump")
+	rev    = flag.Bool("reverse", false, "sort reverse-chronologically (most recent first)")
+	upat   = flag.String("url-regex", "", "print only tabs where URL matches regex")
+	tpat   = flag.String("title-regex", "", "print only tabs where title matches regex")
+	max    = flag.Int("max", -1, "maximum number of tabs printed, -1 for unlimited")
 )
 
 type TabSession []struct {
@@ -88,10 +155,33 @@ func check(err error) {
 }
 
 func main() {
-	input, err := ioutil.ReadFile("/dev/stdin")
+	*tend = time.Now()
+	flag.Var(&TimeValue{tstart}, "start", "dump tabs last accessed on this date and after, RFC 3339 format with optional time and time zone, default to local time (2017-11-01[T00:00:00[-07:00]]) (Default is beginning of file)")
+	flag.Var(&TimeValue{tend}, "end", "dump tabs last accessed on this date and before, in RFC 3339 format with optional time and time zone, default to local time (2017-11-01[T00:00:00[-07:00]]) (Default is end of file)")
+	flag.Parse()
+
+	if tend.Before(*tstart) {
+		panic("range is nonsensical")
+	}
+
+	if (*max == 0) || (*max < -1) {
+		panic("maximum is nonsensical")
+	}
+
+	var ret *regexp.Regexp
+	if len(*tpat) > 0 {
+		ret = regexp.MustCompile(*tpat)
+	}
+
+	var reu *regexp.Regexp
+	if len(*upat) > 0 {
+		reu = regexp.MustCompile(*upat)
+	}
+
+	input, err := ioutil.ReadFile(*inf)
 	check(err)
 
-	output, erro := os.Create("/dev/stdout")
+	output, erro := os.Create(*outf)
 	check(erro)
 	defer output.Close()
 
@@ -101,12 +191,75 @@ func main() {
 	err = json.Unmarshal([]byte(input), &dump)
 	check(err)
 
+	items := make(map[int64]Tab)
+	var keys []int64
 	for i, v := range dump[0].Windows {
 		for j, w := range v {
-			// LastAccessed, URL, Title
-			fmt.Fprintf(writer, "%s\t%s\t%d\t%s\t%s\n", i, j, w.LastAccessed, w.URL, w.Title)
+			stamp := w.LastAccessed/1000
+			items[stamp] = w
+			keys = append(keys, stamp)
 		}
 	}
 
+	if *rev {
+		sort.Slice(keys, func(i, j int) bool { return keys[i] > keys[j] })
+	} else {
+		sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+	}
+
+	st := keys[0]
+	if !tstart.IsZero() {
+		st = tstart.Unix()
+	}
+
+	et := keys[len(keys)-1]
+	if tend.Before(time.Unix(et, 0)) {
+		et = tend.Unix()
+	}
+
+	fmt.Fprintf(writer, "<!DOCTYPE html><html>\n<head><meta charset=\"utf-8\"><title>Session Dump</title></head>\n")
+	fmt.Fprintf(writer, "<body>\n")
+	if *drange {
+		fmt.Fprintf(writer, "<h1>%s - %s</h1>\n", time.Unix(keys[0], 0), time.Unix(keys[len(keys)-1], 0));
+	}
+
+	fmt.Fprintf(writer, "<ol>\n")
+	for _, key := range keys {
+		if key < st {
+			continue
+		}
+
+		if key > et {
+			continue
+		}
+
+		v := items[key]
+
+		if reu != nil {
+			if !reu.Match([]byte(v.URL)) {
+				continue
+			}
+		}
+
+		if *max != -1 {
+			if *max > 0 {
+				*max--
+			} else {
+				break
+			}
+		}
+
+		when := time.Unix(key, 0)
+
+		if ret != nil {
+			if !ret.Match([]byte(v.Title)) {
+				continue
+			}
+		}
+
+		fmt.Fprintf(writer, "<li>%s <a href=\"%s\">%s</a></li>\n", when.Format(time.UnixDate), v.URL, html.EscapeString(v.Title))
+	}
+
+	fmt.Fprintf(writer, "</ol>\n</body></html>")
 	writer.Flush()
 }
